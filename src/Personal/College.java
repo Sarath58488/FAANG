3bccd213aa471ef8585d32793d174541623afc72
Sarath Chintha <sarathchintha04@gmail.com>
2025-12-23 11:19:42 +0530

    Testing second commit

---
+++ src/Personal/College.java
@@ -0,0 +1,11 @@
+package Personal;
+
+public class College {
+    static String CollegeName = "narayana junior college";
+
+    public static void main(String[] args) {
+        College n = new College();
+        System.out.println(n.CollegeName);
+
+    }
+}
